e10f504842235d3ba2e8987f418539f712c234cc
ZMAN <etta1055@gmail.com>
2021-05-28 15:28:13 -0500

    added event listner for modal

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
-
+//for loop looping over nodelist of the show-modals array
 for (let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent);
+btnsOpenModal[i].addEventListener('click', function() {
+    console.log('Button clicked');
+});
+
+
