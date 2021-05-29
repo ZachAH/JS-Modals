fc96e6bf20dd59b8c971e9f6aeae446a6a103c01
ZMAN <etta1055@gmail.com>
2021-05-29 13:45:48 -0500

    completed opening and closing modals and refractered code

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
-//for loop looping over nodelist of the show-modals array
+
+
+//function for opening modals
+const openModal = function () {
+        console.log('Button clicked');
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+}
+
+//for loop looping over nodelist of the show-modals array and calling the openModal function
 for (let i = 0; i < btnsOpenModal.length; i++)
-btnsOpenModal[i].addEventListener('click', function() {
-    console.log('Button clicked');
-});
+btnsOpenModal[i].addEventListener('click', openModal);
+
+
+//function for closing modals adding class hidden back to them
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+} 
+
+//calling the onClick event for closeModal funciton
+btnCloseModal.addEventListener('click', closeModal); 
+overlay.addEventListener('click', closeModal);
 
 
